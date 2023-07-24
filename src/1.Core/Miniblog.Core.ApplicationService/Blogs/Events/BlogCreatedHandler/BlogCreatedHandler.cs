61b68c2e5203e6a501f6e1992c986bdc019b31df
mirzaei.sajad@hotmail.com <mirzaei.sajad@hotmail.com>
2023-07-24 09:39:23 +0330

    Enabled blog event

--- src/1.Core/Miniblog.Core.ApplicationService/Blogs/Events/BlogCreatedHandler/BlogCreatedHandler.cs
+++ src/1.Core/Miniblog.Core.ApplicationService/Blogs/Events/BlogCreatedHandler/BlogCreatedHandler.cs
@@ -3,7 +3,6 @@ using MiniBlog.Core.Contracts.People.Commands;
 using MiniBlog.Core.Domain.Blogs.Events;
 using MiniBlog.Core.Domain.People.Entities;
 using Zamin.Core.Contracts.ApplicationServices.Events;
-using Zamin.Utilities;
 
 namespace MiniBlog.Core.ApplicationService.Blogs.Events.BlogCreatedHandler;
 public class BlogCreatedHandler : IDomainEventHandler<BlogCreated>
@@ -11,21 +10,22 @@ public class BlogCreatedHandler : IDomainEventHandler<BlogCreated>
     private readonly ILogger<BlogCreatedHandler> _logger;
     private readonly IPersonCommandRepository _personCommandRepository;
 
-    public BlogCreatedHandler(ILogger<BlogCreatedHandler> logger ,
-                                IPersonCommandRepository personCommandRepository) 
+    public BlogCreatedHandler(ILogger<BlogCreatedHandler> logger,
+                                IPersonCommandRepository personCommandRepository)
     {
         _logger = logger;
         _personCommandRepository = personCommandRepository;
     }
     public async Task Handle(BlogCreated Event)
     {
-        /*try
+        try
         {
-            Person person = Person
-            {
-                FirstName = DateTime.Now.ToString(),
-                LastName = DateTime.Now.ToLongTimeString(),
-            };
+            var businessId = Guid.NewGuid();
+            Person person = Person.Create(
+                businessId,
+                "Test Event First Name 1 - " + DateTime.Now.ToString(),
+                "Test Event Last Name 1 - " + DateTime.Now.ToLongTimeString()
+            );
             await _personCommandRepository.InsertAsync(person);
             await _personCommandRepository.CommitAsync();
 
@@ -34,9 +34,8 @@ public class BlogCreatedHandler : IDomainEventHandler<BlogCreated>
         }
         catch (Exception ex)
         {
-
             throw;
-        }*/
+        }
 
     }
 }
--- src/3.Endpoints/MiniBlog.Endpoints.API/HostingExtensions.cs
+++ src/3.Endpoints/MiniBlog.Endpoints.API/HostingExtensions.cs
@@ -14,109 +14,109 @@ namespace MiniBlog.Endpoints.API;
 
 public static class HostingExtensions
 {
-	public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
-	{
-		string cnn = "Server=.; Database=ZaminBlog2; User Id=sap; Password=6983; MultipleActiveResultSets = true; Encrypt = false";
-		builder.Services.AddZaminParrotTranslator(c =>
-		{
-			c.ConnectionString = cnn;
-			c.AutoCreateSqlTable = true;
-			c.SchemaName = "dbo";
-			c.TableName = "ParrotTranslations";
-			c.ReloadDataIntervalInMinuts = 1;
-		});
-		builder.Services.AddSingleton<CommandDispatcherDecorator, CustomCommandDecorator>();
-		builder.Services.AddSingleton<QueryDispatcherDecorator, CustomQueryDecorator>();
-		builder.Services.AddSingleton<EventDispatcherDecorator, CustomEventDecorator>();
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        string cnn = "Server=.; Database=ZaminBlog2; User Id=sap; Password=6983; MultipleActiveResultSets = true; Encrypt = false";
+        builder.Services.AddZaminParrotTranslator(c =>
+        {
+            c.ConnectionString = cnn;
+            c.AutoCreateSqlTable = true;
+            c.SchemaName = "dbo";
+            c.TableName = "ParrotTranslations";
+            c.ReloadDataIntervalInMinuts = 1;
+        });
+        builder.Services.AddSingleton<CommandDispatcherDecorator, CustomCommandDecorator>();
+        builder.Services.AddSingleton<QueryDispatcherDecorator, CustomQueryDecorator>();
+        builder.Services.AddSingleton<EventDispatcherDecorator, CustomEventDecorator>();
 
-		builder.Services.AddZaminWebUserInfoService(c => { c.DefaultUserId = "1"; }, true);
+        builder.Services.AddZaminWebUserInfoService(c => { c.DefaultUserId = "1"; }, true);
 
-		builder.Services.AddZaminAutoMapperProfiles(option =>
-		{
-			option.AssmblyNamesForLoadProfiles = "MiniBlog";
-		});
+        builder.Services.AddZaminAutoMapperProfiles(option =>
+        {
+            option.AssmblyNamesForLoadProfiles = "MiniBlog";
+        });
 
-		builder.Services.AddZaminMicrosoftSerializer();
+        builder.Services.AddZaminMicrosoftSerializer();
 
-		builder.Services.AddZaminInMemoryCaching();
+        builder.Services.AddZaminInMemoryCaching();
 
-		builder.Services.AddDbContext<MiniblogCommandDbContext>(c => c.UseSqlServer(cnn).AddInterceptors(new SetPersianYeKeInterceptor(), new AggregateExcludableAddAuditDataInterceptor()));
-		builder.Services.AddDbContext<MiniblogQueryDbContext>(c => c.UseSqlServer(cnn));
+        builder.Services.AddDbContext<MiniblogCommandDbContext>(c => c.UseSqlServer(cnn).AddInterceptors(new SetPersianYeKeInterceptor(), new AggregateExcludableAddAuditDataInterceptor()));
+        builder.Services.AddDbContext<MiniblogQueryDbContext>(c => c.UseSqlServer(cnn));
 
-		builder.Services.AddZaminApiCore("Zamin", "MiniBlog");
-		builder.Services.AddControllers();
-		builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddZaminApiCore("Zamin", "MiniBlog");
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
 
-		//builder.Services.AddZaminTraceJeager(c =>
-		//{
-		//    c.AgentHost = "localhost";
-		//    c.ApplicationName = "MiniBlog";
-		//    c.ServiceName = "OpenTelemetrySample";
-		//    c.ServiceVersion = "1.0.0";
-		//    c.ServiceId = "cb387bb6-9a66-444f-92b2-ff64e2a81f98";
-		//});
+        //builder.Services.AddZaminTraceJeager(c =>
+        //{
+        //    c.AgentHost = "localhost";
+        //    c.ApplicationName = "MiniBlog";
+        //    c.ServiceName = "OpenTelemetrySample";
+        //    c.ServiceVersion = "1.0.0";
+        //    c.ServiceId = "cb387bb6-9a66-444f-92b2-ff64e2a81f98";
+        //});
 
-		//builder.Services.AddZaminRabbitMqMessageBus(c =>
-		//{
-		//    c.PerssistMessage = false;
-		//    c.ExchangeName = "MiniBlogExchange";
-		//    c.ServiceName = "MiniBlogAPI";
-		//    c.Url = @"amqp://guest:guest@localhost:5672/";
-		//});
+        builder.Services.AddZaminRabbitMqMessageBus(c =>
+        {
+            c.PerssistMessage = false;
+            c.ExchangeName = "MiniBlogExchange";
+            c.ServiceName = "MiniBlogAPI";
+            c.Url = @"amqp://guest:guest@localhost:5672/";
+        });
 
-		//builder.Services.AddZaminPollingPublisherDalSql(c =>
-		//{
-		//    c.ApplicationName = "MiniBlog";
-		//    c.ConnectionString = cnn;           
-		//});
+        builder.Services.AddZaminPollingPublisherDalSql(c =>
+        {
+            c.ApplicationName = "MiniBlog";
+            c.ConnectionString = cnn;
+        });
 
-		//builder.Services.AddZaminPollingPublisher(c =>
-		//{
-		//    c.SendInterval = 1000;
-		//    c.SendCount = 100;
-		//    c.ExceptionInterval = 10000;
-		//    c.ApplicationName = "MiniBlog";
-		//});
-
-
-		//builder.Services.AddZaminMessageInbox(c =>
-		//{
-		//    c.ApplicationName = "MiniBlog";
-		//});
-		//builder.Services.AddZaminMessageInboxDalSql(c =>
-		//{
-		//    //c.TableName = "MessageInbox";
-		//    c.SchemaName = "dbo";
-		//    c.ConnectionString = cnn;
-		//});
-
-		builder.Services.AddSwaggerGen();
+        builder.Services.AddZaminPollingPublisher(c =>
+        {
+            c.SendInterval = 1000;
+            c.SendCount = 100;
+            c.ExceptionInterval = 10000;
+            c.ApplicationName = "MiniBlog";
+        });
 
 
+        builder.Services.AddZaminMessageInbox(c =>
+        {
+            c.ApplicationName = "MiniBlog";
+        });
+        builder.Services.AddZaminMessageInboxDalSql(c =>
+        {
+            //c.TableName = "MessageInbox";
+            c.SchemaName = "dbo";
+            c.ConnectionString = cnn;
+        });
+
+        builder.Services.AddSwaggerGen();
 
 
-		return builder.Build();
-	}
 
-	public static WebApplication ConfigurePipeline(this WebApplication app)
-	{
-		app.UseZaminApiExceptionHandler();
-		app.UseSerilogRequestLogging();
 
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
+        return builder.Build();
+    }
 
-		app.UseHttpsRedirection();
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseZaminApiExceptionHandler();
+        app.UseSerilogRequestLogging();
 
-		// app.Services.ReceiveEventFromRabbitMqMessageBus(new KeyValuePair<string, string>("MiniBlog", "BlogCreated"));
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-		app.UseAuthorization();
+        app.UseHttpsRedirection();
 
-		app.MapControllers();
+        app.Services.ReceiveEventFromRabbitMqMessageBus(new KeyValuePair<string, string>("MiniBlog", "BlogCreated"));
 
-		return app;
-	}
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
 }
